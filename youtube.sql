e65c88270c52ca7a2ca925b04abc72a7c4bed076
Jason Tong <jasontong818@gmail.com>
2019-07-08 19:53:24 -0700

    merged two dataframes into sql; merged tables in sql

--- youtube.sql
+++ youtube.sql
@@ -9,4 +9,24 @@ CREATE TABLE videos (
 	comment_total int
 );
 
+CREATE TABLE youtube_category_table(
+    Category_id INT,
+    Channel_id VARCHAR,
+    Category_Title VARCHAR
+);
+
+SELECT * FROM youtube_category_table;
 SELECT * FROM videos;
+
+SELECT videos.video_id, 
+		videos.title, 
+		videos.channel_title, 
+		videos.category_id, 
+		youtube_category_table.category_title, 
+		videos.views, 
+		videos.likes, 
+		videos.dislikes, 
+		videos.comment_total
+FROM videos
+INNER JOIN youtube_category_table
+ON videos.category_id = youtube_category_table.category_id;
